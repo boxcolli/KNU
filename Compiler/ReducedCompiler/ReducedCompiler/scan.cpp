a7cd2a64acc7ccacdde396c24385f7de37658be9
yoonmin <dmg04158@gmail.com>
2022-04-05 21:54:22 +0900

    [Compiler] Scanner Construction Incomplete
    adding states...

--- Compiler/ReducedCompiler/ReducedCompiler/scan.cpp
+++ Compiler/ReducedCompiler/ReducedCompiler/scan.cpp
@@ -2,6 +2,10 @@
 #include <map>
 #include <string>
 
+#define ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+			abcdefghijklmnopqrstuvwxyz"
+#define NUMDIGIT "0123456789"
+
 using namespace std;
 
 class SingleState {
@@ -65,18 +69,64 @@ private:
 
 public:
 	Scanner(ifstream* f) : fin(f), current(nullptr) {
-		addState(string("init"), SingleState(false));
-		addState(string("init"), SingleState(false));
+		// Construct Automata
+		addState("init", SingleState(false));
+		addState("ID", SingleState(false));
+		addState("NUM", SingleState(false));
+		addState("ADD", SingleState(false));
+		addState("SUB", SingleState(false));
+		addState("MUL", SingleState(false));
+		addState("DIV", SingleState(false));
+		addState("LT", SingleState(false));	// w/ LTE
+		addState("GT", SingleState(false));	// w/ GTE
+		addState("EQ", SingleState(false)); // w/ ASSIGN
+		addState("NEQ", SingleState(false));
+		addState("ENDS", SingleState(false));
+		addState("COMMA", SingleState(false));
+		addState("LP", SingleState(false));
+		addState("RP", SingleState(false));
+		addState("LSB", SingleState(false));
+		addState("RSB", SingleState(false));
+		addState("LCB", SingleState(false));
+		addState("RCB", SingleState(false));
+		addState("COMMENT", SingleState(false));
+
+		/// init ///
+		mapState("init", "ID", ALPHABET);
+		mapState("init", "NUM", NUMDIGIT);
+		mapState("init", "ADD", "+");
+		mapState("init", "SUB", "-");
+		mapState("init", "MUL", "*");
+		mapState("init", "DIV", "/");
+		mapState("init", "LT", "<");
+		mapState("init", "GT", ">");
+		mapState("init", "EQ", "=");
+		mapState("init", "NEQ", "!");
+		mapState("init", "ENDS", ";");
+		mapState("init", "COMMA", ",");
+		mapState("init", "LP", "(");
+		mapState("init", "RP", ")");
+		mapState("init", "LSB", "[");
+		mapState("init", "RSB", "]");
+		mapState("init", "LCB", "{");
+		mapState("init", "RCB", "}");
+		mapState("init", "COMMENT", "/");
+
+		/// ID ///
+		
 	}
 
 	void addState(string name, SingleState state) {
 		states[name] = state;
 	}
 
-	void mapState(string from, string to, char in) {
+	void mapState(string from, string to, string ins) {
 		SingleState s1 = states[from];
 		SingleState s2 = states[to];
-		s1.addMap(in, s2);
+		for (char in : ins) {
+			s1.addMap(in, s2);
+		}		
 	}
 
+	
 };
