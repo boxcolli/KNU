eb910b49a56d146b59f5a38a538bc203e78b61b9
yoonmin <dmg04158@gmail.com>
2022-04-05 21:31:26 +0900

    [Compiler] Class Definition
    class SingleState
    class Scanner

--- Compiler/ReducedCompiler/ReducedCompiler/scan.cpp
+++ Compiler/ReducedCompiler/ReducedCompiler/scan.cpp
@@ -1,34 +1,82 @@
+#include <fstream>
 #include <map>
+#include <string>
 
 using namespace std;
 
-enum class SingleResult {
-	error, success
-};
-
 class SingleState {
 private:
-	bool fstate;
-	map<char, SingleState*> table; // input -> nextState mapping
-public:
-	SingleState(bool f) : fstate(f) {}
+	/// <summary>
+	/// True if final state, if else, false.
+	/// </summary>
+	bool finalState;
 
-	void addMap(char in, SingleState* next) {
-		table.insert(make_pair(in, next));
+	/// <summary>
+	/// Map holding transition data
+	/// </summary>
+	map<char, SingleState> transition;
+
+public:
+	/// <summary>
+	/// Class SingleState constructor
+	/// </summary>
+	/// <param name="fin">Given true if this state is final, if else, false</param>
+	SingleState(bool f) : finalState(f) {}
+
+	/// <summary>
+	/// Add a transition from this state to another.
+	/// </summary>
+	/// <param name="in">A single input character</param>
+	/// <param name="next">Pointer to another state</param>
+	void addMap(char in, SingleState next) {
+		transition[in] = next;
 	}
 
-	int input(char in) {
-		auto search = table.find(in);
-		if (search == table.end()) {
-
+	/// <summary>
+	/// Push a single input character to make transition
+	/// </summary>
+	/// <param name="in">A single input character</param>
+	/// <returns>nullptr if not found or mapped. SingleState* if found.</returns>
+	SingleState* pushChar(char in) {
+		auto search = transition.find(in);
+		if (search == transition.end()) {
+			return nullptr;
+		}
+		else {
+			return &(search->second);
 		}
 	}
 
+	/// <summary>
+	/// </summary>
+	/// <returns>True if final state, if else, false</returns>
 	bool isFinal() {
-		return fstate;
+		return finalState;
 	}
 };
 
-class Automata {
+class Scanner {
+private:
+	ifstream* fin;
+
+	map<string, SingleState> states;
+
+	SingleState* current;
+
+public:
+	Scanner(ifstream* f) : fin(f), current(nullptr) {
+		addState(string("init"), SingleState(false));
+		addState(string("init"), SingleState(false));
+	}
+
+	void addState(string name, SingleState state) {
+		states[name] = state;
+	}
+
+	void mapState(string from, string to, char in) {
+		SingleState s1 = states[from];
+		SingleState s2 = states[to];
+		s1.addMap(in, s2);
+	}
 
 };
