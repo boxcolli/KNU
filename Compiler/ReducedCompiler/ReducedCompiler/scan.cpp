d16480ad2823fb6cd2696d8d0b84c26931d09fa0
yoonmin <dmg04158@gmail.com>
2022-04-10 15:27:30 +0900

    [Compiler] scanner processChar()

--- Compiler/ReducedCompiler/ReducedCompiler/scan.cpp
+++ Compiler/ReducedCompiler/ReducedCompiler/scan.cpp
@@ -88,13 +88,19 @@ private:
 
 	map<string, SingleState*> states;
 
-	SingleState* current;
+	// state cursor
+	SingleState* currentState;
 
-	string tokenbuffer;
+	// token buffer
+	string tokenBuffer;
 
-	
+	// flush token buffer on next process
+	bool flushFlag;
 
-	enum StateData {
+	// recent error
+	int errorType;
+
+	typedef enum StateData {
 		nonf = 0,
 		fID, fNUM,
 		fADD, fSUB, fMUL, fDIV,
@@ -102,17 +108,18 @@ private:
 		fASSIGN, fENDS, fCOMMA,
 		fLP, fRP, fLSB, fRSB, fLCB, fRCB
 	};
-	enum TransitionOption {
-		optNORMAL, optLOOKAHEAD
+
+	typedef enum TransitionOption {
+		optNORMAL, optLOOKAHEAD, optDISCARD
 	};
 
-
-
-	void addState(string name, int data=nonf) {
+	void addState(string name, StateData data=nonf) {
 		states[name] = new SingleState(data);
 	}
 
-	void mapState(string from, string to, string instring, int opt=optNORMAL) {
+	void mapState(string from, string to,
+					string instring,
+					TransitionOption opt=optNORMAL) {
 		SingleState* s1 = states[from];
 		SingleState* s2 = states[to];
 		for (char in : instring) {
@@ -142,9 +149,8 @@ private:
 		addState("LCB", fLCB);
 		addState("RCB", fRCB);
 	}
-
 	void buildInit() {
-		mapState("init", "init", WHITESPACE);
+		mapState("init", "init", WHITESPACE, optDISCARD);
 		mapState("init", "ID", ALPHABET);
 		mapState("init", "NUM", NUMDIGIT);
 		mapState("init", "ADD", "+");
@@ -239,26 +245,40 @@ private:
 		mapState("NEQ", "fNEQ", "=");
 	}
 
-	void processChar() {
-		
+	int findKeyword(string token) {
+		if (token == "else") return tELSE;
+		else if (token == "if") return tIF;
+		else if (token == "int") return tINT;
+		else if (token == "return") return tRETURN;
+		else if (token == "void") return tVOID;
+		else if (token == "while") return tWHILE;
+		else return tID;
 	}
 
 public:
 
-
-
-	/// <summary>
-	/// 
-	/// </summary>
-	enum TokenType {
+	typedef enum TokenType {
+		terr = -1,
 		tnull,
 		tELSE, tIF, tINT, tRETURN, tVOID, tWHILE,
+		tID, tNUM,
 		tADD, tSUB, tMUL, tDIV, tLT, tLTE, tGT, tGTE, tEQ, tNEQ,
 		tASSIGN, tENDS, tCOMMA,
 		tLP, tRP, tLSB, tRSB, tLCB, tRCB
 	};
 
-	Scanner(ifstream* f) : fin(f), current(nullptr) {
+	typedef enum ErrorType {
+		eNOERROR,
+		eNOMATCHINGTTYPE,
+		eINVALIDINPUT,
+		eENDOFFILE
+	};
+
+	Scanner(ifstream* f) : fin(f),
+							tokenBuffer(""),
+							flushFlag(false),
+							errorType(eNOERROR) {
+		// build DFA
 		buildState();
 		buildInit();
 		buildNUM();
@@ -267,6 +287,112 @@ public:
 		buildGT_GTE();
 		buildEQ_ASSIGN();
 		buildNEQ();
+
+		currentState = states["init"];
+	}
+
+	TokenType processChar() {
+		char in;
+
+		// EOF?
+		if (!(*fin >> in)) {
+			errorType = eENDOFFILE;
+			return terr;
+		}
+
+		// flush?
+		if (flushFlag) {
+			tokenBuffer.clear();
+			flushFlag = false;
+		}
+
+		// retrieve result from current state
+		auto temp = currentState->pushChar(in);
+		SingleState* nextState = temp.first;
+		int opt = temp.second;
+
+		// no result?
+		if (nextState == nullptr) {
+			// force to reset DFA
+			currentState = states["init"];
+
+			// collect char
+			tokenBuffer.push_back(in);
+
+			// flush next time
+			flushFlag = true;
+
+			// set error
+			errorType = eINVALIDINPUT;
+
+			return terr;
+		}
+
+		// set no error
+		errorType = eNOERROR;
+
+		// process transition option
+		switch (opt) {
+		case optLOOKAHEAD:	
+			fin->putback(in);
+			break;
+		case optDISCARD:
+			// discard
+			break;
+		case optNORMAL:
+			tokenBuffer.push_back(in);
+			break;
+		default:
+			tokenBuffer.push_back(in);
+		}
+
+		// process state data
+		int stateData = nextState->getData();
+		if (stateData == nonf) {
+			return tnull;
+		}
+		else {
+			flushFlag = true;
+			currentState = states["init"];
+			switch (nextState->getData()) {
+			case fID:		return static_cast<TokenType>(findKeyword(tokenBuffer));
+			case fNUM:		return tNUM;
+			case fADD:		return tADD;
+			case fSUB:		return tSUB;
+			case fMUL:		return tMUL;
+			case fDIV:		return tDIV;
+			case fLT:		return tLT;
+			case fLTE:		return tLTE;
+			case fGT:		return tGT;
+			case fGTE:		return tGTE;
+			case fEQ:		return tEQ;
+			case fNEQ:		return tNEQ;
+			case fASSIGN:	return tASSIGN;
+			case fENDS:		return tENDS;
+			case fCOMMA:	return tCOMMA;
+			case fLP:		return tLP;
+			case fRP:		return tRP;
+			case fLSB:		return tLSB;
+			case fRSB:		return tRSB;
+			case fLCB:		return tLCB;
+			case fRCB:		return tRCB;
+			default:
+				errorType = eNOMATCHINGTTYPE;
+				return terr;
+			}
+		}
+	}
+
+	void processToken() {
+
+	}
+
+	string getToken() {
+		return tokenBuffer;
+	}
+
+	ErrorType getErrorType() {
+		return static_cast<ErrorType>(errorType);
 	}
 };
 
