6793fe4182d2911e2d4e30dc31240f4bf7ebf065
yoonmin <dmg04158@gmail.com>
2022-05-09 15:05:59 +0900

    [SWT] driver memo

--- SWT/auto_test/Gen Drive/Gen Drive/scan.cpp
+++ SWT/auto_test/Gen Drive/Gen Drive/scan.cpp
@@ -3,7 +3,7 @@
 
 #include "global.h"
 
-typedef enum class Keyw {
+typedef enum class KW {
 	NIL,
 	SIG, USIG,
 	C, S, I, L, F, D,
@@ -11,65 +11,65 @@ typedef enum class Keyw {
 	Count // to determine number of elements
 };
 
-Keyw mapKeyw(string tok) {
-	if (tok == "signed") return Keyw::SIG;
-	if (tok == "unsigned") return Keyw::USIG;
-	if (tok == "char") return Keyw::C;
-	if (tok == "short") return Keyw::S;
-	if (tok == "int") return Keyw::I;
-	if (tok == "long") return Keyw::L;
-	if (tok == "float") return Keyw::F;
-	if (tok == "double") return Keyw::D;
-	if (tok == "void") return Keyw::V;
-	return Keyw::NIL;
+KW mapKeyw(string tok) {
+	if (tok == "signed") return KW::SIG;
+	if (tok == "unsigned") return KW::USIG;
+	if (tok == "char") return KW::C;
+	if (tok == "short") return KW::S;
+	if (tok == "int") return KW::I;
+	if (tok == "long") return KW::L;
+	if (tok == "float") return KW::F;
+	if (tok == "double") return KW::D;
+	if (tok == "void") return KW::V;
+	return KW::NIL;
 }
 
-int ktoi(Keyw k) {
+int ktoi(KW k) {
 	return static_cast<int>(k);
 }
 
-DType mapDType(vector<Keyw> klist) {
+DType mapDType(vector<KW> klist) {
 	// count keyword
-	vector<int> kcount(ktoi(Keyw::Count), 0);
-	for (Keyw k : klist) {
+	vector<int> kcount(ktoi(KW::Count), 0);
+	for (KW k : klist) {
 		kcount[ktoi(k)]++;
 	}
 
 
 
 	// map
-	if (kcount[ktoi(Keyw::C)] == 1) {
-		if (kcount[ktoi(Keyw::SIG)] == 1) return DType::SC;
-		if (kcount[ktoi(Keyw::USIG)] == 1) return DType::UC;
+	if (kcount[ktoi(KW::C)] == 1) {
+		if (kcount[ktoi(KW::SIG)] == 1) return DType::SC;
+		if (kcount[ktoi(KW::USIG)] == 1) return DType::UC;
 		return DType::C;
 	}
 
-	if (kcount[ktoi(Keyw::S)] == 1) {
-		if (kcount[ktoi(Keyw::USIG)] == 1) return DType::US;
+	if (kcount[ktoi(KW::S)] == 1) {
+		if (kcount[ktoi(KW::USIG)] == 1) return DType::US;
 		return DType::S;
 	}
 
-	if (kcount[ktoi(Keyw::I)] == 1) {
-		if (kcount[ktoi(Keyw::USIG)] == 1) {
-			if (kcount[ktoi(Keyw::L)] == 1) return DType::UL;
-			if (kcount[ktoi(Keyw::L)] == 2) return DType::ULL;
+	if (kcount[ktoi(KW::I)] == 1) {
+		if (kcount[ktoi(KW::USIG)] == 1) {
+			if (kcount[ktoi(KW::L)] == 1) return DType::UL;
+			if (kcount[ktoi(KW::L)] == 2) return DType::ULL;
 			return DType::UI;
 		}
-		if (kcount[ktoi(Keyw::L)] == 1) return DType::L;
-		if (kcount[ktoi(Keyw::L)] == 2) return DType::LL;
+		if (kcount[ktoi(KW::L)] == 1) return DType::L;
+		if (kcount[ktoi(KW::L)] == 2) return DType::LL;
 		return DType::I;
 	}
 
-	if (kcount[ktoi(Keyw::F)] == 1) return DType::F;
+	if (kcount[ktoi(KW::F)] == 1) return DType::F;
 
-	if (kcount[ktoi(Keyw::D)] == 1) {
-		if (kcount[ktoi(Keyw::L)] == 1) return DType::LD;
+	if (kcount[ktoi(KW::D)] == 1) {
+		if (kcount[ktoi(KW::L)] == 1) return DType::LD;
 		return DType::D;
 	}
 
-	if (kcount[ktoi(Keyw::L)] == 1) return DType::L;
+	if (kcount[ktoi(KW::L)] == 1) return DType::L;
 
-	if (kcount[ktoi(Keyw::L)] == 2) return DType::LL;
+	if (kcount[ktoi(KW::L)] == 2) return DType::LL;
 
 	return DType::Error;
 }
@@ -84,11 +84,11 @@ FunScanner::FunScanner(string funDecLine) : fundec(funDecLine) {
 
 
 	// 1. return type
-	vector<Keyw> ret_klist;
+	vector<KW> ret_klist;
 	while (tok) {		
-		Keyw k = mapKeyw(tok);		// string -> enum
+		KW k = mapKeyw(tok);		// string -> enum
 		tok = strtok(str, delim);	// prepare next token
-		if (k == Keyw::NIL) break;	// not keyword (function name)
+		if (k == KW::NIL) break;	// not keyword (function name)
 		ret_klist.push_back(k);		// flag[enum]++
 	}
 	retType = mapDType(ret_klist);
@@ -107,11 +107,11 @@ FunScanner::FunScanner(string funDecLine) : fundec(funDecLine) {
 
 
 	// 3. arg type
-	vector<Keyw> arg_klist;
+	vector<KW> arg_klist;
 	while (tok) {
-		Keyw k = mapKeyw(tok);
+		KW k = mapKeyw(tok);
 
-		if (k == Keyw::NIL) {
+		if (k == KW::NIL) {
 			argType.push_back(mapDType(arg_klist));
 			arg_klist.clear();
 		}
