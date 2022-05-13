31ad31c406e838aca890dcce1ddf031410f76a86
rollas <dmg04158@gmail.com>
2022-05-13 18:40:30 +0900

    [Compiler] state.h

--- Compiler/ReducedCompiler/ReducedCompiler/state.cpp
+++ Compiler/ReducedCompiler/ReducedCompiler/state.cpp
@@ -1,10 +1,3 @@
 #include "globals.h"
 #include "state.h"
 
-template<class T>
-class ListState : FiniteState {
-private:
-	vector<T> l;
-public:
-	ListState(T l);
-};
--- Compiler/ReducedCompiler/ReducedCompiler/state.h
+++ Compiler/ReducedCompiler/ReducedCompiler/state.h
@@ -5,6 +5,37 @@
 
 template <typename Data, typename In, typename Opt>
 class FiniteState {
+public:
+
+	FiniteState(Data data) : data(data) {}
+
+	/// Add a transition from this state to another.
+	/// in: A single input character
+	/// next: Pointer to another state
+	template <typename State>
+	void addMap(In in, State* next, Opt opt) {
+		transition[in] = make_pair(next, opt);
+	}
+
+	/// Push a single input character to make transition
+	/// in: A single input character</param>
+	/// returns nullptr if not found or mapped. FiniteState* if found.
+	template <typename State>
+	pair<State*, Opt> pushInput(In in) {
+		auto search = transition.find(in);
+		if (search == transition.end()) {
+			// not found
+			return make_pair(nullptr, -1);
+		}
+		else {
+			// return value
+			return search->second;
+		}
+	}
+
+	/// returns assigned data for this state.
+	DT getData() { return data; }
+	
 private:
 
 	/// Can hold any optional data. For example, non-final, final or else.
@@ -13,32 +44,6 @@ private:
 	/// Holds transition data, with transition option
 	map<char, pair<FiniteState*, Opt>> transition;
 
-public:
-
-	FiniteState(Data data) : data(data) {}
-
-	/// Add a transition from this state to another.
-	/// in: A single input character
-	/// next: Pointer to another state
-	void addMap(In in, FiniteState* next, Opt opt) {
-		transition[in] = make_pair(next, opt);
-	}
-
-	/// Push a single input character to make transition
-	/// in: A single input character</param>
-	/// returns nullptr if not found or mapped. FiniteState* if found.
-	pair<FiniteState*, int> pushInput(In in) {
-		auto search = transition.find(in);
-		if (search == transition.end()) {
-			return make_pair(nullptr, -1);
-		}
-		else {
-			return search->second;
-		}
-	}
-
-	/// returns assigned data for this state.
-	DT getData() { return data; }
 };
 
 #endif
