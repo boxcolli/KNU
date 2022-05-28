e73c76f87edc405eb19d6d83e57a73ac7f34dfd4
rollas <dmg04158@gmail.com>
2022-05-29 07:33:49 +0900

    minor update

--- IR/SimpleIRSystem/analyze.py
+++ IR/SimpleIRSystem/analyze.py
@@ -1,38 +1,46 @@
 from konlpy.tag import Okt
-okt = Okt()
 
+okt = Okt()
 
 stopwordstring = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
 
+sampleline = '사랑하고싶게하는가슴속온감정을헤집어놓는영화예요정말최고 헷 km 1245 #%$&'
+
+
+# ---- File --------------------------------------- #
 def file_to_string(filename):
     return open(filename, 'r', encoding='utf-8').read()
 
 
-def is_title(line):
+def file_to_lines(filename):
+    return open(filename, 'r', encoding='utf-8').readlines()
+
+
+# ---- String --------------------------------------- #
+
+
+def str_is_title(line):
     if '<title>' in line:
         return True
     else:
         return False
 
 
-def get_title(line):
+def str_get_title(line):
     return line.replace('<title>', '').replace('</title>', '')
 
 
-def get_simple_line(line):
+def str_get_simple_line(line):
     for c in stopwordstring:
         line = line.replace(c, ' ')
     return line
 
-
-def
-
-
-def tag_me(txt):
-    return okt.pos(txt, norm=True, stem=True, join=True)
+def str_morph_me(txt):
+    return okt.morphs(txt, norm=True, stem=True)
 
 
-def print_tags(txt):
-    for item in tag_me(txt):
-        print(item)
+def print_list(list):
+    for item in list:
+        print(list)
+
--- IR/SimpleIRSystem/main.py
+++ IR/SimpleIRSystem/main.py
@@ -6,6 +6,11 @@ import os
 print('using:', os.environ.get('JAVA_HOME'))
 
 import analyze as an
+from tqdm import tqdm
+
+filename = 'example.txt'
+term_dict = dict()
+
 
 single = '<title>1. 지미 카터</title>\
 지미 카터는 민주당 출신 미국 39번째 대통령이다.\
@@ -24,10 +29,55 @@ double = '<title>1. 지미 카터</title>\
 체첸에서 사용되는 언어는 체첸어와 러시아어이다. \
 체첸어는 캅카스제어 중, 북동 캅카스제어로 불리는 그룹에 속하는데 인구시어와 매우 밀접한 관계에 있다.'
 
-an.printTags(single)
 
 
-#---- Split Document ----#
-#---- Tag and Insert ----#
+
+
+def push_index(str, doc_i):
+    if str == '':
+        return
+
+    # make index of this string
+    for stem in an.str_morph_me(an.str_get_simple_line(str)): # get stems
+        # new entry?
+        if stem not in term_dict:
+            term_dict[stem] = dict()
+
+        term_indices = term_dict[stem]
+
+        # new doc entry?
+        if doc_i not in term_indices:
+            term_indices[doc_i] = 1
+        else:
+            term_indices[doc_i] += 1
+
+
+lines = an.file_to_lines(filename)
+buffer = ''
+doc_num = 0
+
+
+with tqdm(total=2) as pbar:
+    for line in lines:
+        if an.str_is_title(line):
+            # ---- Tag and Insert ----#
+            push_index(buffer, doc_num)
+            buffer = an.str_get_title(line)
+            doc_num += 1
+            pbar.update(1)
+        else:
+            buffer += line
+            # ---- Split Document ----#
+    push_index(buffer, doc_num)
+    doc_num += 1
+
+print(term_dict)
+for term, index in term_dict.items():
+    print(f'indices of [{term}]')
+    for doc, count in index.items():
+        print('\t', doc, count)
+
+
+
 #---- Count Term ----#
 #----
